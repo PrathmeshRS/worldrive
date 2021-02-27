baa61b2a916785a0f9511c01382780481131910a
Prathmesh S <shindeprathmesh99@gmail.com>
2021-02-27 19:18:37 +0530

    Updates

---
+++ precache-manifest.44e3932edf3c4d564f0ff884c3ac6bfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcf9059edd5f573fccd9",
+    "url": "/worldrive/static/css/main.3d8b7ef3.chunk.css"
+  },
+  {
+    "revision": "bcf9059edd5f573fccd9",
+    "url": "/worldrive/static/js/main.bcf9059e.chunk.js"
+  },
+  {
+    "revision": "b26cebad07fbdcf01c24",
+    "url": "/worldrive/static/js/1.b26cebad.chunk.js"
+  },
+  {
+    "revision": "82b4084ead309aabcfc9",
+    "url": "/worldrive/static/js/runtime~main.82b4084e.js"
+  },
+  {
+    "revision": "707eead196c807af8566b67fcc704ec0",
+    "url": "/worldrive/static/media/earth.707eead1.png"
+  },
+  {
+    "revision": "e5d85cf68e4067d99301479a9aeb43d0",
+    "url": "/worldrive/index.html"
+  }
+];
